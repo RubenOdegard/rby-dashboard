c1be7868c556a358c28950a0644776c376a346c9
RubenOdegard <ruben.odegard@gmail.com>
2024-04-16 13:08:46 +0200

    Updated config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,22 +5,22 @@ import { loadEnvConfig } from "@next/env";
 loadEnvConfig(cwd());
 
 if (!process.env.TURSO_DATABASE_URL) {
-  throw new Error("DATABASE_URL is not set");
+    throw new Error("DATABASE_URL is not set");
 }
 
 if (!process.env.TURSO_AUTH_TOKEN) {
-  throw new Error("TURSO_DATABASE_URL is not set");
+    throw new Error("TURSO_DATABASE_URL is not set");
 }
 
 export default {
-  schema: "./src/db/schema.ts",
-  out: "./migrations",
-  driver: "turso",
-  verbose: true,
-  dbCredentials: {
-    url: process.env.TURSO_DATABASE_URL,
-    authToken: process.env.TURSO_AUTH_TOKEN,
-  },
+    schema: "./src/db/schema.ts",
+    out: "./migrations",
+    driver: "turso",
+    verbose: true,
+    dbCredentials: {
+        url: process.env.TURSO_DATABASE_URL,
+        authToken: process.env.TURSO_AUTH_TOKEN,
+    },
 } satisfies Config;
 
 // turso db shell turso-prisma-db < ./prisma/migrations/20230922132717_init/migration.sql
