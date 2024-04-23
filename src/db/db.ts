1709a20b836942ea064db06428fdf64c9ed70998
RubenOdegard <ruben.odegard@gmail.com>
2024-04-23 23:27:48 +0200

    Working on local db, with specified file I can use npm run db:push

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,16 +3,17 @@ import { createClient } from "@libsql/client";
 
 // Handle env variable errors
 if (!process.env.TURSO_DATABASE_URL) {
-  throw new Error("TURSO_DATABASE_URL is not set");
+    throw new Error("TURSO_DATABASE_URL is not set");
 }
 if (!process.env.TURSO_AUTH_TOKEN) {
-  throw new Error("TURSO_AUTH_TOKEN is not set");
+    throw new Error("TURSO_AUTH_TOKEN is not set");
 }
 
 // Merge configurations based on environment
 export const connection = {
-  url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+    // url: process.env.TURSO_DATABASE_URL,
+    url: "file:local.db",
+    authToken: process.env.TURSO_AUTH_TOKEN,
 };
 
 // Create new client with the merged configuration
