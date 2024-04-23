44db028eb7882dedde90a3b8d80f4b3418ed1d63
RubenOdegard <ruben.odegard@gmail.com>
2024-04-23 23:25:43 +0200

    Updated providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,34 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import LayoutProvider from "./layout-provider";
-import AuthProvider from "./auth-provider";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import AuthProvider from "@/app/_providers/auth-provider";
+import LayoutProvider from "@/app/_providers/layout-provider";
 
 export const metadata: Metadata = {
-	title: "Developer Dashboard",
-	description: "A developer dashboard to manage your tools and dependencies.",
+    title: "Developer Dashboard",
+    description: "A developer dashboard to manage your tools and dependencies.",
 };
 
 export default async function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning={true}>
-			<body
-				className={cn(
-					"relative min-h-screen bg-background font-sans antialiased",
-					GeistSans.variable,
-					GeistMono.variable,
-				)}
-			>
-				<AuthProvider>
-					<LayoutProvider>{children}</LayoutProvider>
-				</AuthProvider>
-			</body>
-		</html>
-	);
+    return (
+        <html lang="en" suppressHydrationWarning={true}>
+            <body
+                className={cn(
+                    "relative min-h-screen bg-background font-sans antialiased",
+                    GeistSans.variable,
+                    GeistMono.variable,
+                )}
+            >
+                <AuthProvider>
+                    <LayoutProvider>{children}</LayoutProvider>
+                </AuthProvider>
+            </body>
+        </html>
+    );
 }
