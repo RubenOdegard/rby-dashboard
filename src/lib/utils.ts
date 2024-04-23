5a3417ae8b36efd9e5d08a1089bfa33b69c5da9d
RubenOdegard <ruben.odegard@gmail.com>
2024-04-23 23:29:16 +0200

    Updated utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,30 +4,36 @@ import moment from "moment";
 import { toast } from "sonner";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export function capitalizeFirstLetter(string: string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 export function getDomainName(url: string) {
-	const domain = new URL(url).hostname;
-	return domain;
+    const domain = new URL(url).hostname;
+    return domain;
 }
 
 export const generateRandomId = () => {
-	return Math.floor(Math.random() * 1000000);
+    return Math.floor(Math.random() * 1000000);
 };
 
 export function toastSuccess(string: string) {
-	return toast.success(string, {
-		description: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
-	});
+    return toast.success(string, {
+        description: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
+    });
 }
 
 export function toastError(string: string) {
-	return toast.error(string, {
-		description: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
-	});
+    return toast.error(string, {
+        description: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
+    });
 }
+
+export function getProjectIDFromURL(selectedProject: string, projects: any) {
+    const projectId = projects.find((project: any) => project.project === selectedProject)?.id;
+    return projectId
+}
+
